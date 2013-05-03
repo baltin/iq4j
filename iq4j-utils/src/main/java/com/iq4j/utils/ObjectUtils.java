ddf487ef030577ca3c1f73a8af9c5248c7865fcb
Sertac ANADOLLU <sertac@anadollu.com>
2013-05-03 11:31:39 +0300

    ObjectUtils.copy(Obj instance) changed to copy(T instance)

--- iq4j-utils/src/main/java/com/iq4j/utils/ObjectUtils.java
+++ iq4j-utils/src/main/java/com/iq4j/utils/ObjectUtils.java
@@ -3,6 +3,7 @@ package com.iq4j.utils;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 public class ObjectUtils {
 
@@ -18,13 +19,15 @@ public class ObjectUtils {
 	/**
 	 * Returns a copy of the object, or null if the object cannot be serialized.
 	 */
-	public static Object copy(Object orig) {
+	@SuppressWarnings("unchecked")
+	public static <T extends Serializable> T copy(T original) {
 		Object obj = null;
+		
 		try {
 			// Write the object out to a byte array
 			FastByteArrayOutputStream fbos = new FastByteArrayOutputStream();
 			ObjectOutputStream out = new ObjectOutputStream(fbos);
-			out.writeObject(orig);
+			out.writeObject(original);
 			out.flush();
 			out.close();
 
@@ -37,7 +40,8 @@ public class ObjectUtils {
 		} catch (ClassNotFoundException cnfe) {
 			cnfe.printStackTrace();
 		}
-		return obj;
+		
+		return (T) obj;
 	}
 
 	
