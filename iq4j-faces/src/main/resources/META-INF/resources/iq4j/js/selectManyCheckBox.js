03cfade16daff3c9e66909af7794da4abb1b6529
baltint <b.altinterim@gmail.com>
2013-06-05 11:56:50 +0300

    selectManyCheckBox component refactored.

--- iq4j-faces/src/main/resources/META-INF/resources/iq4j/js/selectManyCheckBox.js
+++ iq4j-faces/src/main/resources/META-INF/resources/iq4j/js/selectManyCheckBox.js
@@ -1,18 +1,19 @@
 function checkAll(e) {
 	
 	var checked = e.checked;
-	var findBox = $(e).parents(".cmp_smc_gr").find(".ui-chkbox-box");
-	var findIcon = $(e).parents(".cmp_smc_gr").find(".ui-chkbox-icon");
-	var checkbox = $(e).parents(".cmp_smc_gr").find("input[type=checkbox]");
+	var smcgr = $(e).parents(".cmp_smc_gr");
+	var box = smcgr.find(".ui-chkbox-box");
+	var icon = smcgr.find(".ui-chkbox-icon");
+	var checkbox = smcgr.find("input[type=checkbox]");
 	
 	if (!checked) {
 		checkbox.removeAttr("checked");
-		findBox.removeClass("ui-state-active");
-		findIcon.removeClass("ui-icon ui-icon-check");
+		box.removeClass("ui-state-active");
+		icon.removeClass("ui-icon ui-icon-check");
 	} else {
 		checkbox.attr("checked", "checked");
-		findBox.addClass("ui-state-active");
-		findIcon.addClass("ui-icon ui-icon-check");
+		box.addClass("ui-state-active");
+		icon.addClass("ui-icon ui-icon-check");
 	}
 	
 }
@@ -21,9 +22,10 @@ function checkItem(e) {
 
 	var checked = e.checked;
 	var checkbox = $(e).parents(".cmp_blck").find("input[type=checkbox]");
-	var box = $(e).parents(".cmp_smc_gr").find(".cmp_smc_ca .ui-chkbox-box");
-	var icon = $(e).parents(".cmp_smc_gr").find(".cmp_smc_ca .ui-chkbox-icon");
-	var upperBox = $(e).parents(".cmp_smc_gr").find(".cmp_smc_ca div input[type=checkbox]");
+	var smcgr = $(e).parents(".cmp_smc_gr");
+	var box = smcgr.find(".cmp_smc_ca .ui-chkbox-box");
+	var icon = smcgr.find(".cmp_smc_ca .ui-chkbox-icon");
+	var upperBox = smcgr.find(".cmp_smc_ca div input[type=checkbox]");
 	var checkList = [];
 	
 	if(!checked) {
@@ -62,7 +64,7 @@ function initCheckAll(e) {
 		}
 	});
 
-	if(!list.length > 0) {
+	if(list.length == 0) {
 		upper.attr("checked", "checked");
 		box.addClass("ui-state-active");
 		icon.addClass("ui-icon ui-icon-check");
